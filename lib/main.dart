03cfe859fa9a61a6324073b7d2faa284522d014a
SilverGhost95 <64695203+SilverGhost95@users.noreply.github.com>
2021-04-25 21:19:20 -0500

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+    home: Home(),
+  ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Press Go Button"),
+        centerTitle: true,
+        backgroundColor: Colors.blue[600],
+      ),
+      body: Center(
+          child: IconButton(
+            onPressed: (){
+              print('Click me');
+            },
+            icon: Icon(Icons.alternate_email),
+            color: Colors.amber,
+          ),
+        ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Text("Go"),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
